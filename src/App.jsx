d77c5b48834382b7d8c878e96a17f35f370dc73e
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-03-04 13:31:26 +0200

    add

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Home from './Home'
+import Users from './Users'
+
+const App = () => {
+  return (
+    <div className="page">
+      <Router>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users" component={Users} />
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.jsx',
+    output: {
+      filename: 'bundle.js',
+     publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/i,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.s?css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(jpg|png|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                name: '[name].[ext]',
+                outputPath: 'images',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    devServer: {
+      port: 9000,
+      hot: true,
+      historyApiFallback:true
+    },
+  };
+  if (isProduction) {
+    config.plugins.push(
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    );
+  }
+  return config;
+};
